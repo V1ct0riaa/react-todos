f262c41f60e626f46cf6b49df16831981583d22b
Natanael Pascal <npascal70@gmail.com>
2026-05-30 23:30:46 +0700

    feat:adding TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import './App.css'
 
+import TodoList from './TodoList'
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <CssBaseline />
       <h1>Todos</h1>
+      <TodoList/>
     </>
   )
 }
