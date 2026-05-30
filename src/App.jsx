f217f3f124b09f7fe992c7e4e856ab03dcddf6f6
Natanael Pascal <npascal70@gmail.com>
2026-05-30 22:57:18 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+    </>
+  )
+}
+
+export default App
